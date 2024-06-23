f0fc7ed66d070637d942c042a258c7125c21d679
kundakchan <kundakchan@yandex.ru>
2024-06-23 15:12:20 +0300

    feat: добавил ресурсы

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, AuthModule],
-  providers: [AuthService],
+  imports: [AuthModule, UsersModule, ProductsModule],
 })
 export class AppModule {}
